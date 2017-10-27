23fae7ccbf6f51462f54a0678051ddebae931c2a
Marcel <marcelkroker@mkrd-MacBook.local>
2017-10-27 11:51:12 +0200

    Initial Commit

---
+++ Swift Developer Tools/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Developer Tools
+//
+//  Created by Marcel Kröker on 27.10.17.
+//  Copyright © 2017 Marcel Kröker. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
